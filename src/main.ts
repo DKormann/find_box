6fedc5d7db8a84ff2e4731a8dbb7503dc26e2864
DKormann <dominikormann@outlook.com>
2025-11-24 16:29:27 +0100

    buffertype experiment

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ function log(el:any){
 
 
 const blockSize = "40px";
-type Color = 0 | 1 | 2 | 3
+
 const colors = ["black", "red", "green", "#0044FF"]
 
 function show_block(block: number){
@@ -51,20 +51,6 @@ function show_block(block: number){
 
 
 
-
-
-type DataType = {
-  atom : "block" | "color" | "value"
-  shape : "scalar" | "mat"
-}
-
-
-const value_arr: DataType = {
-  atom: "value",
-  shape: "mat"
-}
-
-
 function to_shape(x: any, shape: number[]){
   if (shape.length == 0) return x
   if (x instanceof Array)return x.map(y => to_shape(y, shape.slice(1)))
@@ -82,192 +68,140 @@ function broadcast(x: any, y: any){
   return [to_shape(x, s2), to_shape(y, s1)]
 }
 
-
-type Rule ={
-  arity: number,
-  exec: (inT: DataType[]) => {
-    T: DataType
-    f: (ins: any[]) => any
-  }
-} | null
+type Atom = "block" | "color" | "value"
+type Shape = "scalar" | "arr"
+type Color = 0 | 1 | 2 | 3
 
 
 
-
-let new_field: Rule = ({arity: 0, exec:(Ts: DataType[]) => {
-
-  return {
-    T:{
-      atom: "block",
-      shape: "mat"
-    },
-    f: () => Array.from({length: 4}, () => Array.from({length: 4}, () => 0))
-  }
-}})
-
-
-
-function atom_view(T:DataType['atom'], x: number){
-
-  let color = "white";
-  let val = "#";
-  if (T == "color") color = colors[x]
-  if (T == "block") {
-    let cn = x == 0 ? 0 : (x-1) % 3 + 1
-    val = cn > 0 ? String((x - cn) / 3 + 1) : "";
-    color = colors[cn]
-  }
-  if (T == "value") {
-    val = String(x)
-  }
-
-  return div({
-    style:{
-      width: blockSize,
-      height: blockSize,
-      color,
-      "text-align": "center",
-      "font-size": blockSize,
-      "font-weight": "bold",
-    },
-  }, val)
+type BufferType = {
+  atom : "block" | "color" | "value"
+  shape : "scalar" | "arr"
 }
 
 
-function mat_view(T: DataType['atom'], x: any){
-  return div({
-    style:{
-      display: "flex",
-      "flex-wrap": "wrap",
-      width: `calc(${blockSize} * 4)`,
-      margin: "auto",
-      "background": "#111",
-      "border": "1px solid white",
-    }
-  }, x.map(row => div(...row.map(block => atom_view(T, block))))
-  )
+function BufferType(atom: Atom, shape: Shape): BufferType{return {atom, shape}}
+const ScalarBlockType = BufferType("block", "scalar")
+const ScalarColorType = BufferType("color", "scalar")
+const ScalarValueType = BufferType("value", "scalar")
+const ArrBlockType = BufferType("block", "arr")
+const ArrColorType = BufferType("color", "arr")
+const ArrValueType = BufferType("value", "arr")
+
+
+const ScalarConst = (T: BufferType, value: number): Buffer => ({ tag: "buffer", T,  run: () => value})
+
+type Buffer = {
+  tag: "buffer"
+  T: BufferType
+  run: () => number | number[]
 }
 
 
 
 
-const View : Rule = ({arity: 1, exec: ([T]: DataType[]) => {
-  return {
-    T,
-    f: ([x]: any) =>{
-      if (T.shape == "scalar") put(atom_view(T.atom, x))
-      else put(mat_view(T.atom, x))
-    }
-  }
-}})
+type Fun = {
+  tag: "fun"
+  try: (x: BufferType) => Ast | null
+}
+
+type Ast = Buffer | Fun
+
+const ID: Ast = {
+  tag: "fun",
+  try: (b: Buffer) => b
+}
+
+const RED: Ast = ScalarConst(ScalarColorType, 1)
+const C1: Ast = ScalarConst(ScalarValueType, 1)
+const RED1: Ast = ScalarConst(ScalarBlockType, 1)
 
 
-
-let _get_color: Rule = ({arity: 1, exec: ([{atom, shape}]: DataType[]) => {
-
-  if (shape == "mat") return null
-  if (atom == "block") {
+const s_EQ: Ast = {
+  tag: "fun",
+  try: (x: Buffer) =>{
+    if (x.T.shape != "scalar") return null
     return {
-      T: {
-        atom: "color",
-        shape: "scalar"
-      },
-      f: ([x]) => x == 0 ? 0 : (x-1) % 3 + 1
+      tag: "fun",
+      try: (y: Buffer) =>{
+        if (x.T.atom == y.T.atom && x.T.shape == y.T.shape){
+          return {
+            tag: "buffer",
+            T:  ScalarValueType,
+            run: () => String(x.run()) == String(y.run()) ? 1 : 0
+          }
+        }
+        return null
+      }
+    }
+  }
+}
+
+const s_unary = (f: (x: number)=> number): Ast => {
+  return {
+    tag: "fun",
+    try: (x: Buffer) => {
+      if (x.T.shape != "scalar") return null
+      return {
+        tag: "buffer",
+        T: ScalarValueType,
+        run: () => f(x.run() as number)
+      }
+    }
+  }
+}
+
+
+
+const dummy = (T: BufferType): Buffer => ({
+  tag: "buffer",
+  T,
+  run: () => {throw new Error("Dummy buffer")}
+})
+
+
+const unary = (f: Fun) => {
+  return {
+    tag: "fun",
+    try: (x: Buffer) => {
+      if (x.T.shape == "scalar") return f.try(x)
+      if (x.T.shape == "arr"){
+        let T : BufferType = {...x.T, shape: "scalar"};
+        let frun = f.try(dummy(T));
+        if (frun == null) return null;
+        { return {
+          tag : "buffer",
+          T: {
+            shape: "arr",
+            atom: x.T.atom,
+          },
+          run: () => (x.run() as number[]).map(x=>f.try(
+        }
+      }}
+    }
+  }
+}
+
+
+const s_add_1 : Ast = s_unary(x => x + 1)
+
+log(s_add_1)
+
+
+function app(a: Ast, b: Ast): Ast | null{
+  if (a.tag == "fun"){
+    if (b.tag == "buffer"){
+      return a.try(b)
+    }else{
+      return {
+        tag: "fun",
+        try: (x: Buffer) => app(a, b.try(x))
+      }
     }
   }
   return null
-}})
-
-
-function unaryop(op: Rule) : Rule{
-  return ({arity: 1, exec: ([inT]: DataType[]) => {
-
-    let opR = op.exec([
-      {
-        atom: inT.atom,
-        shape: "scalar"
-      }
-    ])
-
-    if (opR == null) return null
-
-    return {
-      T: {
-        atom: opR.T.atom,
-        shape: inT.shape
-      },
-      f: ([x]) => {
-        let go = (x: any)=>{
-          if (typeof x == "number"){
-            return opR.f([x])
-          }
-          return x.map((xi: any) => go(xi))
-        }
-        return go(x)
-      }
-    }
-  }})
 }
 
 
-function binop(elementary: Rule) : Rule {
-  if (elementary.arity != 2) throw new Error("elementary rule must have arity 2")
-  return {arity: 2, exec: ([d1, d2]: DataType[]) => {
-    let R = elementary.exec([
-      {atom: d1.atom, shape: "scalar"},
-      {atom: d2.atom, shape: "scalar"}
-    ])
-    if (R == null) return null
-
-    function go(x: any, y: any){
-      if (typeof x == "number" && typeof y == "number"){
-        return R.f([x, y])
-      }
-      return x.map((xi: any, i: number) => go(xi, y[i]))
-    }
-    return {
-      T:{
-        atom: R.T.atom,
-        shape: (d1.shape == d2.shape) ? d1.shape : "mat"
-      },
-      f: ([x, y]) => {
-        [x, y] = broadcast(x, y)
-        return go(x, y)
-      }
-    }
-  }
-}}
-
-
-
-
-function apply(a:Rule, b:Rule) : Rule{
-  if (b.arity == 0){
-
-    let bR = b.exec([])
-    
-    if (a.arity == 1){
-      
-      let aR = a.exec([bR.T])
-      if (aR == null) return null
-      return {
-        arity: 0,
-        exec: () => aR.f([bR.f([])])
-      }
-    }
-  }else{
-    return {
-      arity: a.arity + b.arity - 1,
-      exec: (args) => {
-        
-      }
-    }
-  }
-}
-
-let show0: Rule= apply(View, new_field)
-
-show0.exec([])
-
-
+let x = app(s_add_1, app(s_add_1, C1))
 
