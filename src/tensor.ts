f85a59b599eb81d98c43c6b6f74cebf498573709
DKormann <dominikormann@outlook.com>
2025-12-03 14:07:15 +0100

    legoo

--- src/tensor.ts
+++ src/tensor.ts
@@ -13,30 +13,29 @@ export type Atom = [string, ...Atom[]];
 export type Tensor = [DataType, ...Atom[]]
 export const dtype = ([dt, ..._]: Tensor): DataType => dt.split("_")[0] as DataType
 export type Fun = ((...x:Tensor[]) => Tensor | null)
-export const alusrc = (i: number) => [`L[${i}]`]
 const atom = (op: string, ...srcs: Atom[]):Atom => [op, ...srcs]
 
-const scalar = (x: number, type: DataType) : Fun => ()=> [type, [x.toString()]]
-const aluunary = (alu: string, type: DataType): Fun => ([t,...a]: Tensor) => [type, ...a.map(a=>atom(alu, a))]
-const cast = (X: Tensor, T: DataType): Tensor | null => {
-  if (X[0] == T) return X;
-  if (T == "block") return null;
-  if (T == "boolean") return aluunary("($0 ? 1 : 0)", "boolean")(X);
-  if (X[0] == "number" && T == "color") return aluunary("($0 > 3 ? 4 : $0)", "color")(X);
-  if (X[0] == "color" && T == "number") return aluunary("($0 == 0 ? 0 : ($0-1) % 3 + 1)", "number")(X);
-  if (X[0] == "block" && T == "number") return aluunary("($0 == 0 ? 0 : (($0+2) / 3) | 0)", "number")(X);
-  if (X[0] == "block" && T == "color") return aluunary("($0 == 0 ? 0 : ($0-1) % 3 + 1)", "color")(X);
+const fun = (alu: string, type: DataType | null, ...srcs: Tensor[]): Tensor =>{
+  let as = srcs.map(([t, ...a]:Tensor)=>a);
+  return [type,...range(Math.max(...as.map(as=>as.length))).map(i=>atom(alu, ...as.map(at=>at[i % at.length] as Atom)))]
 }
 
-const alubin = (alu: string, type: DataType | null): Fun => ([ta, ...a]:Tensor, [tb, ...b]:Tensor) => 
-  [type || ta, ...range(Math.max(a.length, b.length)).map(i=> atom(alu, a[i % a.length], b[i % b.length]))]
+const cast = (X: Tensor, T: DataType): Tensor | null =>  (X[0] == T) ? X:
+  (T == "block") ? null:
+  (T == "boolean") ? fun("($0 ? 1 : 0)", "boolean", X):
+  (X[0] == "number" && T == "color") ? fun("($0 > 3 ? 4 : $0)", "color", X):
+  (X[0] == "color" && T == "number") ? fun("($0 == 0 ? 0 : ($0-1) % 3 + 1)", "number", X):
+  (X[0] == "block" && T == "number") ? fun("($0 == 0 ? 0 : (($0+2) / 3) | 0)", "number", X):
+  (X[0] == "block" && T == "color") ? fun("($0 == 0 ? 0 : ($0-1) % 3 + 1)", "color", X):
+  null;
+
 
 const reduce = (def: Atom, alu: string, type: DataType): Fun => ([ta, ...a]:Tensor) => [type, a.reduce((acc,x)=> atom(alu, acc, x), def)]
-const eq: Fun = (a:Tensor, b:Tensor) => dtype(a) != dtype(b) ? null : alubin("($0 == $1)", "boolean")(a, b)
-const eq_poly: Fun = (a:Tensor, b:Tensor) => alubin(dtype(a) != dtype(b) ? "0" : "($0 == $1)", "boolean")(a, b)
-const add: Fun = (a:Tensor, b:Tensor) => dtype(a) == "color" || dtype(b) == "color" ? null : alubin("($0 + $1)", "number") (cast(a, "number"), cast(b, "number"))
-const mul: Fun = (a:Tensor, b:Tensor) => dtype(a) == "color" || dtype(b) == "color" ? null : alubin("($0 * $1)", "number") (cast(a, "number"), cast(b, "number"))
-const and: Fun = (a:Tensor, b:Tensor) => alubin("($0 && $1)", "boolean")(cast(a, "boolean"), cast(b, "boolean"))
+const eq: Fun = (a:Tensor, b:Tensor) => dtype(a) != dtype(b) ? null : fun("($0 == $1)", "boolean", a, b)
+const add: Fun = (a:Tensor, b:Tensor) => dtype(a) == "color" || dtype(b) == "color" ? null : fun("($0 + $1)", "number", cast(a, "number"), cast(b, "number"))
+const mul: Fun = (a:Tensor, b:Tensor) => dtype(a) == "color" || dtype(b) == "color" ? null : fun("($0 * $1)", "number", cast(a, "number"), cast(b, "number"))
+const and: Fun = (a:Tensor, b:Tensor) => fun("($0 && $1)", "boolean", cast(a, "boolean"), cast(b, "boolean"))
+const eq_poly: Fun = (a:Tensor, b:Tensor) => fun(dtype(a) != dtype(b) ? "0" : "($0 == $1)", "boolean", a, b)
 const sum: Fun = (a:Tensor) => dtype(a) == "color" ? null : reduce(atom("0"), "($0 + $1)", "number")(cast(a, "number"))
 const any: Fun = (a:Tensor) => reduce(atom("0"), "($0 || $1)", "boolean")(a)
 const all: Fun = (a:Tensor) => reduce(atom("1"), "($0 && $1)", "boolean")(a)
@@ -57,28 +56,14 @@ const chain = (...fs: Fun[]) : Tensor => {
     let x = range(f.length).map(_ => go());
     if (x.some(x=>x == null)) throw new Error("Null argument");
     let r = f(...x);
-    if (r == null) {
-      print("null result", f, x)
-      throw new Error("Null result");
-    }
+    if (r == null) print("null result", f, x)
     return r;
   }
   return go();
 }
 
 export const compile = (rule: Fun[]) : [DataType, "matrix" | "scalar", (L: Int32Array) => Int32Array] => {
-
-  print("compile", print_rule(rule))
-
-  let T: DataType;
-  let atoms: Atom[];
-
-  try{
-    [T, ...atoms] = chain(...rule);
-  }catch(e){
-    print("compile error", rule, e.message)
-    throw e;
-  }
+  let [T, ...atoms] = chain(...rule);
   let usecount = new Map<string, number>();
   let count = ([op, ...a]: Atom) => {
     let key = JSON.stringify([op, ...a]);
@@ -99,7 +84,7 @@ export const compile = (rule: Fun[]) : [DataType, "matrix" | "scalar", (L: Int32
       let name = `x${seen.size}`;
       seen.set(key, name);
       code += `const ${name} = ${c};\n`;
-      return key;
+      return name;
     }
     return c;
   }
@@ -108,7 +93,7 @@ export const compile = (rule: Fun[]) : [DataType, "matrix" | "scalar", (L: Int32
   return [T, atoms.length == 1 ? "scalar" : "matrix", new Function("L", code) as (L: Int32Array) => Int32Array]
 }
 
-const is_color = (x: number): Fun => (a:Tensor) => eq( cast(a, "color"), scalar(x, "color")())
+const is_color = (x: number): Fun => (a:Tensor) => eq( cast(a, "color"), ["color", atom(x.toString())])
 
 export let Core : Record<string, Fun> = {
   number: (a:Tensor) => cast(a, "number"),
@@ -117,18 +102,16 @@ export let Core : Record<string, Fun> = {
   isgreen: is_color(2),
   isblue: is_color(3),
   any, all, sum,
-  not: (a:Tensor) => dtype(a) != "boolean" ? null : aluunary("(!$0)", "boolean")(a),
+  not: (a:Tensor) => dtype(a) != "boolean" ? null : fun("(!$0)", "boolean", a),
   and, eq, add, mul,
-  "0": scalar(0, "number"),
-  "1": scalar(1, "number"),
-  "2": scalar(2, "number"),
-  "3": scalar(3, "number"),
+  "0": ()=>["number", atom("0")],
+  "1": ()=>["number", atom("1")],
+  "2": ()=>["number", atom("2")],
+  "3": ()=>["number", atom("3")],
   x:() => ["block", ...range(mat_size).map(i=> atom(`L[${i}]`))]
 }
 
-const print_rule = (rule: Fun[]) => rule.map(f=>Object.entries(Lang).find(([k, v])=>v == f)?.[0] || f )
-
-const or = (a:Tensor, b:Tensor) => dtype(a) != "boolean" || dtype(b) != "boolean" ? null : alubin("($0 || $1)", "boolean")(a, b)
+const or = (a:Tensor, b:Tensor) => dtype(a) != "boolean" || dtype(b) != "boolean" ? null : fun("($0 || $1)", "boolean", a, b)
 const right = move_dir(1, 0)
 const up = move_dir(0, -1)
 const left = move_dir(-1, 0)
@@ -136,14 +119,12 @@ const down = move_dir(0, 1)
 export let Lang : Record<string, Fun> = {
   _eq: eq_poly,
   ...Core,
-  or,
-  right, up, left, down,
+  or, right, up, left, down,
   next: (x:Tensor) => or(or(right(x), up(x)), or(left(x), down(x))),
-  block: (num:Tensor, color:Tensor) => dtype(num) != "number" || dtype(color) != "color" ? null :
-    alubin("($0 == 0 ? 0 : ($0*3)-2 + $1 -1)", "block")(num, color),
-  red : scalar(1, "color"),
-  green : scalar(2, "color"),
-  blue : scalar(3, "color"),
+  block: (num:Tensor, color:Tensor) => dtype(num) != "number" || dtype(color) != "color" ? null : fun("($0 == 0 ? 0 : ($0*3)-2 + $1 -1)", "block", num, color),
+  red : ()=>["color", ["1"]],
+  green : ()=>["color", ["2"]],
+  blue : ()=>["color", ["3"]],
 }
 
 {
