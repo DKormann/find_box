840e6fddd429553fc2247ccd2d4e486b07045175
DKormann <dominikormann@outlook.com>
2025-12-04 22:08:33 +0100

    schedule

--- src/compilejs.ts
+++ src/compilejs.ts
@@ -1,3 +1,30 @@
-export const compile = (uops: UOp[]) => {
+import { Op, Op0, Op1, Op2, Op3, UOp, Schedule } from "./uop";
 
+
+const symbols: Record<Op, string> = {
+  add : "($0+$1)", mul : "($0*$1)", and : "($0&&$1)", or : "($0||$1)", eq : "($0==$1)",
+  lt : "($0<$1)", idiv : "($0/$1 |0)", inv : "($0^1)", not : "(!$0)", load : "L[$0]",
+  const : "$0", ifelse : "($0 ? $1 : $2)", neg : "(-$0)", mod : "($0%$1)"}
+
+
+export const compile = (x:Schedule[]) => {
+
+  let code = "";
+
+  let seen = new Map<UOp, string>();
+  let raster = (x:Schedule) => {
+    if (seen.has(x)) return seen.get(x);
+    let c = x.srcs.reduce((p, c, i) => p.replaceAll(`$${i}`, raster(c)), symbols[x.op])
+    if (x.uses > 1) {
+      let name = `x${seen.size}`;
+      seen.set(x, name);
+      code += `const ${name} = ${c};\n`;
+      return name;
+    }
+    return c;
+  }
+
+  let ret = x.map(raster).join(",\n");
+  code = code + `return [${ret}];`;
+  return Function("L", code) as (L: Int32Array) => Int32Array;
 }
--- src/uop.ts
+++ src/uop.ts
@@ -1,17 +1,14 @@
-import { print } from "./html"
-
-
-type Op0 = "load" | "const"
-type Op1 = "neg" | "inv" | "not"
-type Op2 = "add" | "mul" | "and" | "or" | "eq" | "lt" | "idiv" | "mod"
-type Op3 = "ifelse"
+export type Op0 = "load" | "const"
+export type Op1 = "neg" | "inv" | "not"
+export type Op2 = "add" | "mul" | "and" | "or" | "eq" | "lt" | "idiv" | "mod"
+export type Op3 = "ifelse"
 
 export const op1 : Op1[] = ["neg", "inv", "not"]
 export const op2 : Op2[] = ["add", "mul", "and", "or", "eq", "lt", "idiv", "mod"]
 export const op3 : Op3[] = ["ifelse"]
 
-type Op = Op0 | Op1 | Op2 | Op3
-export type UOp = {op: Op1 | Op2 | Op3, args: UOp[]} | {op: Op0, arg: number}
+export type Op = Op0 | Op1 | Op2 | Op3
+export type UOp = {op: Op1 | Op2 | Op3, args: UOp[]} | {op: Op0, arg: number, args: []}
 
 export type minimath<T> =
   Record<Op0, (n: number) => T>
@@ -34,3 +31,27 @@ const miniuop : minimath<UOp> = {
 }
 
 export const UOp = FULLMATH<UOp>(miniuop)
+
+
+export type Schedule = UOp & {uses: number, srcs: Schedule[]}
+
+const schedule = (x:UOp[]): Schedule[] => {
+  let cache = new Map<string, number>();
+  let li : Schedule[] = [];
+  let go = (x:UOp): number => {
+    let srcs = x.args.map(go);
+    let key = JSON.stringify([x.op, ...srcs])
+    if (cache.has(key)) return cache.get(key);
+    cache.set(key, cache.size);
+    li.push({...x, uses: 0, srcs: srcs.map(i=>li[i])})
+    return cache.size -1;
+  }
+  li.forEach(x=>x.srcs.forEach(s=>s.uses++));
+  return li;
+}
+
+import { compile as compile_js } from "./compilejs";
+
+export const compile = (x:UOp[]): (L: Int32Array) => Int32Array =>  compile_js(schedule(x));
+
+
